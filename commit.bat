6aa6ba284bcab2e2ba152d609a97d7ce8c265a8c
Lamastronote <mathieu.victor.petrovitch@gmail.com>
2025-12-09 22:21:37 +0100

    commit by command

--- commit.bat
+++ commit.bat
@@ -1,5 +1,9 @@
 git add .
-git commit -m %1 || "commit by command"
+if "%1" == "" (
+  git commit -m "commit by command"
+) else (
+  git commit -m %1
+)
 git branch -M main
 git remote add origin https://github.com/Lamastronotte/Machionia.git
 git push -u origin main
